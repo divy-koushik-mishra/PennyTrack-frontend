b177f96db0a9cfec49fa5c05492c011e0025ace0
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-09 01:53:25 +0530

    homepage-completed

--- components/landing-page/Home.jsx
+++ components/landing-page/Home.jsx
@@ -1,5 +1,82 @@
 import React from "react";
+import { Button } from "@material-tailwind/react";
+import Link from "next/link";
+import Image from "next/image";
+import phone from "@/public/phone.png";
+import chart from "@/public/chart.png";
 
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<div className="">
+			{/* Hero Section */}
+			<div className="grid place-items-center gap-6 mb-8" id="hero">
+				<div className="text-[#b8b8b8] w-fit rounded-lg bg-[#1c1c1c] p-4 hover:scale-110 transition ease-in-out duration-200 cursor-pointer">
+					Next generation of
+				</div>
+				<h1 className="text-8xl text-transparent font-medium bg-gradient-to-b from-[#f0f0f0] to-[#fff] bg-clip-text">
+					expense tracking.
+				</h1>
+				<div className="">
+					<Image src={phone} alt="phone-image" />
+				</div>
+				<p className="text-xl text-center max-w-[43rem] text-[#eee]">
+					PennyTrack is the ultimate tool for managing your money and staying on
+					top of your expenses. With our easy-to-use app, you can quickly input
+					and categorize your expenses, set and track a budget.
+				</p>
+				<div className="grid grid-cols-2 items-center gap-4">
+					<Link href="#buttons-with-link">
+						<Button size="lg">Log In</Button>
+					</Link>
+					<Link href="#buttons-with-link">
+						<Button
+							variant="outlined"
+							size="lg"
+							className="text-black hover:ring ring-gray-500 bg-light-green-500"
+						>
+							Sign Up
+						</Button>
+					</Link>
+				</div>
+			</div>
+			{/* Features Section*/}
+			<div className="grid place-items-center gap-6" id="features">
+				<h1 className="text-8xl text-transparent font-medium bg-gradient-to-b from-[#f0f0f0] to-[#fff] bg-clip-text">
+					Features.
+				</h1>
+				<div className="grid grid-cols-2 justify-center items-center">
+					<div className="">
+						<Image src={chart} alt="chart" className="" />
+					</div>
+					<p className="text-xl text-center text-[#eee] max-w-[50rem]">
+						Welcome to our PennyTrack, where managing your finances has never
+						been easier. With our platform, you can effortlessly track your
+						expenses, ensuring that every dollar is accounted for. Whether
+						it&apos;s groceries, bills, or leisure activities, our intuitive
+						interface allows you to input and monitor your spending with ease.
+						Take control of your financial future by setting savings goals
+						tailored to your needs. Our system ensures you never miss a payment
+						deadline again, sending timely reminders to keep you on track and
+						avoid unnecessary fees. Visualize your financial progress with
+						dynamic charts that provide insights into your spending habits and
+						savings trends. And to keep you informed every step of the way,
+						receive convenient email notifications for important updates and
+						reminders. Start your journey towards financial freedom today with
+						our comprehensive PennyTrack, an expense tracker website.
+					</p>
+				</div>
+			</div>
+			{/* About Section */}
+			<div className="grid place-items-center gap-6 pb-[5rem] " id="about">
+				<h1 className="text-8xl text-transparent font-medium bg-gradient-to-b from-[#f0f0f0] to-[#fff] bg-clip-text">
+					About.
+				</h1>
+				<p className="text-xl text-center text-[#eee] max-w-lg">
+					PennyTrack is an expense tracker website built by final year college
+					students. What once was a pet project turned into a real world
+					application
+				</p>
+			</div>
+		</div>
+	);
 }
--- components/landing-page/LPNav.jsx
+++ components/landing-page/LPNav.jsx
@@ -8,8 +8,7 @@ import {
 } from "@material-tailwind/react";
 import React from "react";
 import Home from "./Home";
-import About from "./About";
-import Features from "./Features";
+import Link from "next/link";
 
 export default function TabBar() {
 	const [activeTab, setActiveTab] = React.useState("home");
@@ -18,25 +17,37 @@ export default function TabBar() {
 			label: "Home",
 			value: "home",
 			desc: <Home />,
-		},
-		{
-			label: "About Us",
-			value: "about-us",
-			desc: <About />,
+			link: "hero",
 		},
 		{
 			label: "Features",
 			value: "features",
-			desc: <Features />,
+			desc: "",
+			link: "features",
+		},
+		{
+			label: "About Us",
+			value: "about-us",
+			desc: "",
+			link: "about",
 		},
 	];
+
+	const renderContent = (desc, link) => {
+		if (desc) {
+			return desc;
+		} else {
+			return <Link href={`#${link}`}></Link>;
+		}
+	};
+
 	return (
 		<div className="">
 			<Tabs value={activeTab}>
 				<TabsHeader
 					className="flex justify-center items-center bg-gray-800 p-4 w-1/4 mx-auto my-8"
 					indicatorProps={{
-						className: `absolute inset-0 z-10 h-full bg-green-400 rounded-md shadow px-8 align-middle`,
+						className: `absolute inset-0 z-10 h-full bg-light-green-500 rounded-md shadow px-8 align-middle`,
 					}}
 				>
 					{data.map(({ label, value }) => (
@@ -51,9 +62,9 @@ export default function TabBar() {
 					))}
 				</TabsHeader>
 				<TabsBody>
-					{data.map(({ value, desc }) => (
+					{data.map(({ value, desc, link }) => (
 						<TabPanel key={value} value={value}>
-							{desc}
+							{renderContent(desc, link)}
 						</TabPanel>
 					))}
 				</TabsBody>
