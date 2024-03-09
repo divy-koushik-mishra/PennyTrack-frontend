5828f706587197afda3196af03cf2369b29781e5
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-09 14:06:20 +0530

    login-input-tweaks

--- pages/login.jsx
+++ pages/login.jsx
@@ -30,7 +30,7 @@ export default function login() {
 				</CardHeader>
 				<CardBody className="flex flex-col gap-4">
 					<Input label="Email" size="lg" color="white" />
-					<Input label="Password" size="lg" color="white" />
+					<Input label="Password" size="lg" color="white" type="password" />
 					<div className="-ml-2.5">
 						<Checkbox
 							label="Remember Me"
