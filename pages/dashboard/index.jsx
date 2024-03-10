16103382db6e89e9b1ba295dbe8df0c9d6915475
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-10 20:32:08 +0530

    [dashboard]-homepage-layout-fixed

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,14 +1,20 @@
+import IEBCards from "@/components/dashboard-page/IEBCards";
 import { Sidebar } from "@/components/dashboard-page/Sidebar";
 import Topbar from "@/components/dashboard-page/Topbar";
 import React from "react";
 
 export default function Dashboard() {
 	return (
-		<div className="flex gap-x-4">
-			<div className="">
-				<Sidebar />
+		<div className="flex flex-col h-screen">
+			<div className="flex">
+				<div className="">
+					<Sidebar />
+				</div>
+				<div className="flex flex-col w-full space-x-6">
+					<Topbar />
+					<IEBCards />
+				</div>
 			</div>
-			<Topbar />
 		</div>
 	);
 }
