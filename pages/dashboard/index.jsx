02fe9d488dc9a785d18886b017b1f25b35fc8cc6
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-10 19:21:27 +0530

    [dashboard]-sidebar-pages-added

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,12 @@
+import { Sidebar } from "@/components/dashboard-page/Sidebar";
+import React from "react";
+
+export default function Dashboard() {
+	return (
+		<div className="">
+			<div className="">
+				<Sidebar />
+			</div>
+		</div>
+	);
+}
