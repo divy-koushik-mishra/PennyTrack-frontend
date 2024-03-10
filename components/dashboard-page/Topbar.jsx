3330b5650da61c1c654f8f8f8de33abc9b5507fc
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-10 20:38:07 +0530

    [dashboard]-topbar-height-fixed

--- components/dashboard-page/Topbar.jsx
+++ components/dashboard-page/Topbar.jsx
@@ -3,7 +3,7 @@ import { Avatar } from "@material-tailwind/react";
 
 export default function Topbar() {
 	return (
-		<div className="flex justify-between items-center p-8 mb-2 h-24 w-full text-light-green-500">
+		<div className="flex justify-between items-center p-8 h-28 w-full text-light-green-500">
 			<h1 className="text-3xl font-medium">Welcome to PennyTrack Dashboard</h1>
 			<div className="w-[16rem] bg-[#1d1d1d] px-4 py-2.5 rounded-lg">
 				<div className="flex gap-x-6 items-center">
