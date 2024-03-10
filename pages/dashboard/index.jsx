274cbeb77169b2c1c89ce378262c6fc8a741ff5a
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-10 19:42:56 +0530

    [dashboard]-homepage-components-added

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,12 +1,14 @@
 import { Sidebar } from "@/components/dashboard-page/Sidebar";
+import Topbar from "@/components/dashboard-page/Topbar";
 import React from "react";
 
 export default function Dashboard() {
 	return (
-		<div className="">
+		<div className="flex gap-x-4">
 			<div className="">
 				<Sidebar />
 			</div>
+			<Topbar />
 		</div>
 	);
 }
