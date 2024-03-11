a4e6f86f13c335a5f93d6208a9b944e773a1ac20
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-11 21:20:31 +0530

    [transactions]-page-completed

--- pages/dashboard/transactions.jsx
+++ pages/dashboard/transactions.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { Sidebar } from "@/components/dashboard-homepage/Sidebar";
+import { TransactionsPageTable } from "@/components/dashboard-transaction/TransactioPageTable";
+import TransactionTopbar from "@/components/dashboard-transaction/TransactionTopbar";
 
 export default function transactions() {
-	return <div>transactions</div>;
+	return (
+		<div className="flex flex-col">
+			<div className="flex">
+				<div className="sticky">
+					<Sidebar />
+				</div>
+				<div className="flex flex-col w-full space-x-6">
+					<TransactionTopbar />
+					<TransactionsPageTable />
+					<div className="flex gap-x-4 pt-6"></div>
+					<div className="flex gap-x-4 pt-6 mb-4"></div>
+				</div>
+			</div>
+		</div>
+	);
 }
