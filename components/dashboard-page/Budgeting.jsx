cbb35232d80cdada509ec89cb15d5d74a887e435
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-11 09:45:51 +0530

    [dashboard]-budgeting-component-added

---
+++ components/dashboard-page/Budgeting.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Budgeting() {
+	return (
+		<div className="">
+			<div className="bg-[#1d1d1d] w-[40.5rem] h-full p-8 rounded-xl grid place-items-center">
+				<h1 className="text-3xl text-light-green-500 font-medium">Budgeting</h1>
+			</div>
+		</div>
+	);
+}
