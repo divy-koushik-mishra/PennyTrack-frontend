e9421cfad2026dc0713e52b846fed7b74ea1cbc0
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-14 10:45:29 +0530

    [dashboard]-income-page-completed

--- pages/dashboard/income.jsx
+++ pages/dashboard/income.jsx
@@ -1,5 +1,20 @@
+import { Sidebar } from "@/components/dashboard/Sidebar";
+import IncomeTabs from "@/components/dashboard/Income/IncomeTabs";
+import IncomeTopbar from "@/components/dashboard/Income/IncomeTopbar";
 import React from "react";
 
 export default function income() {
-	return <div>income</div>;
+	return (
+		<div className="flex flex-col">
+			<div className="flex">
+				<div className="min-h-screen">
+					<Sidebar />
+				</div>
+				<div className="flex flex-col w-full space-x-6">
+					<IncomeTopbar />
+					<IncomeTabs />
+				</div>
+			</div>
+		</div>
+	);
 }
