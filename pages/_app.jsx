58f0346d2d6ea73cb99220f5fc66c59a14d6672c
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-14 22:08:16 +0530

    added-title-to-the-page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,12 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	const renderWithLayout =
+		Component.getLayout ||
+		function (page) {
+			return <Layout>{page}</Layout>;
+		};
+
+	return renderWithLayout(<Component {...pageProps} />);
 }
