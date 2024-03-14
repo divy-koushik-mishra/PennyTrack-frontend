1a76241c43028056d30130678600ed095f3a5c01
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-14 22:14:33 +0530

    [dashboard]-budget-page

--- pages/dashboard/budgeting.jsx
+++ pages/dashboard/budgeting.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import BudgetTabs from "@/components/dashboard/Budgeting/BudgetTabs";
+import BudgetTopbar from "@/components/dashboard/Budgeting/BudgetTopbar";
+import { Sidebar } from "@/components/dashboard/Sidebar";
 
-export default function budgeting() {
-	return <div>budgeting</div>;
+export default function expenses() {
+	return (
+		<div className="flex flex-col">
+			<div className="flex">
+				<div className="min-h-screen">
+					<Sidebar />
+				</div>
+				<div className="flex flex-col w-full space-x-6">
+					<BudgetTopbar />
+					<BudgetTabs />
+				</div>
+			</div>
+		</div>
+	);
 }
