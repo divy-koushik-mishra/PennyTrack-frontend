b3956b6ba681282325c329570f66703611530622
Divy Koushik Mishra <divykoushikmishra@gmail.com>
2024-03-16 13:42:20 +0530

    Add useState and useRouter hooks to login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,70 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import {
-	Card,
-	CardHeader,
-	CardBody,
-	CardFooter,
-	Typography,
-	Input,
-	Checkbox,
-	Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Checkbox,
+  Button,
 } from "@material-tailwind/react";
 import Link from "next/link";
 
-export default function login() {
-	return (
-		<div className="flex flex-col justify-center items-center h-screen gap-10 mx-auto">
-			<h1 className="font-semibold text-4xl bg-gradient-to-r from-light-green-500 to-light-green-200 text-transparent bg-clip-text">
-				Welcome Back to PennyTrack
-			</h1>
-			{/* Login Card */}
+export default function Login() {
+  const router = useRouter();
 
-			<Card className="w-96 bg-[#1c1c1c] text-white">
-				<CardHeader
-					variant="gradient"
-					color="gray"
-					className="mb-4 grid h-28 place-items-center"
-				>
-					<Typography variant="h3" color="light-green">
-						Log In
-					</Typography>
-				</CardHeader>
-				<CardBody className="flex flex-col gap-4">
-					<Input label="Email" size="lg" color="white" />
-					<Input label="Password" size="lg" color="white" type="password" />
-					<div className="-ml-2.5">
-						<Checkbox
-							label="Remember Me"
-							color="light-green"
-							className="text-white"
-							labelProps={{ className: `text-light-green-500` }}
-						/>
-					</div>
-				</CardBody>
-				<CardFooter className="pt-0">
-					<Link href="/dashboard/">
-						<Button
-							variant="gradient"
-							className="text-light-green-500"
-							fullWidth
-						>
-							Log In
-						</Button>
-					</Link>
-					<Typography variant="small" className="mt-6 flex justify-center">
-						Don&apos;t have an account?
-						<Typography
-							as="a"
-							href="/signup"
-							variant="small"
-							color="light-green"
-							className="ml-1 font-bold"
-						>
-							Sign up
-						</Typography>
-					</Typography>
-				</CardFooter>
-			</Card>
-		</div>
-	);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    if (!email || !password) {
+      alert("Please fill all the fields");
+      e.preventDefault();
+      return;
+    }
+
+    e.preventDefault();
+    console.log(email, password);
+    //  api post request
+    axios
+      .post("http://localhost:8000/api/v1/users/login", {
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res.data.data.accessToken);
+
+        if (res.status === 200) {
+          console.log(res);
+
+          localStorage.setItem("accessToken", res.data.data.accessToken);
+          localStorage.setItem("refreshToken", res.data.data.refreshToken);
+          localStorage.setItem("username", res.data.data.user.username);
+          alert("User logged in successfully");
+          router.push("/dashboard");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+
+        alert("Invalid credentials");
+      });
+  };
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-10 mx-auto">
+      <h1 className="font-semibold text-4xl bg-gradient-to-r from-light-green-500 to-light-green-200 text-transparent bg-clip-text">
+        Welcome Back to PennyTrack
+      </h1>
+      {/* Login Card */}
+
+      <Card className="w-96 bg-[#1c1c1c] text-white">
+        <CardHeader
+          variant="gradient"
+          color="gray"
+          className="mb-4 grid h-28 place-items-center"
+        >
+          <Typography variant="h3" color="light-green">
+            Log In
+          </Typography>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4">
+          <Input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email"
+            size="lg"
+            color="white"
+          />
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password"
+            size="lg"
+            color="white"
+            type="password"
+          />
+          <div className="-ml-2.5">
+            <Checkbox
+              label="Remember Me"
+              color="light-green"
+              className="text-white"
+              labelProps={{ className: `text-light-green-500` }}
+            />
+          </div>
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Link href="/dashboard/">
+            <Button
+              variant="gradient"
+              className="text-light-green-500"
+              fullWidth
+            >
+              Log In
+            </Button>
+          </Link>
+          <Typography variant="small" className="mt-6 flex justify-center">
+            Don&apos;t have an account?
+            <Typography
+              as="a"
+              href="/signup"
+              variant="small"
+              color="light-green"
+              className="ml-1 font-bold"
+            >
+              Sign up
+            </Typography>
+          </Typography>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
