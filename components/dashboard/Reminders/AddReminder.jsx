4bb21845e1c8ba9dba0c662a994723c95c8e34ae
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-17 19:29:02 +0530

    integrated-apis-in-add-and-view-reminders

--- components/dashboard/Reminders/AddReminder.jsx
+++ components/dashboard/Reminders/AddReminder.jsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function AddReminder() {
+	const [formData, setFormData] = useState({
+		title: "",
+		amount: "",
+	});
+	const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/reminder/addReminder`;
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			[name]: value,
+		}));
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const accessToken = localStorage.getItem("accessToken");
+			if (!accessToken) {
+				return;
+			}
+
+			const config = {
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			};
+
+			const response = await axios.post(url, formData, config);
+
+			console.log("Reminder added successfully:", response.data);
+
+			setFormData({
+				title: "",
+				amount: "",
+			});
+		} catch (error) {
+			console.error("Error adding reminder:", error);
+		}
+	};
+
 	return (
 		<div className="text-white">
-			<form className="lg:flex flex-col m-4 lg:mx-[20rem]">
+			<form
+				className="lg:flex flex-col m-4 lg:mx-[20rem]"
+				onSubmit={handleSubmit}
+			>
 				<div className="">
 					{/* Desc */}
 					<div className="mb-6">
 						<p className="text-lg mb-6">Reminder Description</p>
 						<input
 							type="text"
+							name="title"
+							value={formData.title}
+							onChange={handleChange}
 							className="w-full h-[3.25rem] bg-[#292929] border border-[#333333] rounded-md p-4"
 						/>
 					</div>
@@ -18,6 +66,9 @@ export default function AddReminder() {
 						<p className="text-lg mb-6">Reminder Amount</p>
 						<input
 							type="number"
+							name="amount"
+							value={formData.amount}
+							onChange={handleChange}
 							className="w-full h-[3.25rem] bg-[#292929] border border-[#333333] rounded-md p-4"
 						/>
 					</div>
@@ -29,7 +80,10 @@ export default function AddReminder() {
 							className="w-full h-[3.25rem] bg-[#292929] border border-[#333333] rounded-md p-4"
 						/>
 					</div>
-					<button className="w-full flex justify-center items-center bg-gradient-to-r from-light-green-800 via-light-green-500 to-light-green-300 text-black text-xl font-medium rounded-[0.25rem] py-[0.6875rem] pr-4 pl-6 transition duration-300 ease-in-out hover:shadow-top-md hover:shadow-bottom-md">
+					<button
+						type="submit"
+						className="w-full flex justify-center items-center bg-gradient-to-r from-light-green-800 via-light-green-500 to-light-green-300 text-black text-xl font-medium rounded-[0.25rem] py-[0.6875rem] pr-4 pl-6 transition duration-300 ease-in-out hover:shadow-top-md hover:shadow-bottom-md"
+					>
 						Add Reminder
 					</button>
 				</div>
--- components/dashboard/Reminders/ViewReminders.jsx
+++ components/dashboard/Reminders/ViewReminders.jsx
@@ -1,143 +1,85 @@
-import { PencilIcon } from "@heroicons/react/24/solid";
 import {
-	Avatar,
-	Button,
 	Card,
 	CardBody,
-	CardFooter,
 	CardHeader,
 	Chip,
-	IconButton,
-	Tooltip,
 	Typography,
 } from "@material-tailwind/react";
-
-const TABLE_HEAD = [
-	"Transaction",
-	"Amount",
-	"Date",
-	"Status",
-	"Type",
-	"Account",
-	"",
-];
-
-const TABLE_ROWS = [
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-spotify.svg",
-		name: "Spotify",
-		amount: "$2,500",
-		date: "Wed 3:00pm",
-		status: "paid",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-amazon.svg",
-		name: "Amazon",
-		amount: "$5,000",
-		date: "Wed 1:00pm",
-		status: "paid",
-		account: "master-card",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-pinterest.svg",
-		name: "Pinterest",
-		amount: "$3,400",
-		date: "Mon 7:40pm",
-		status: "pending",
-		account: "master-card",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-google.svg",
-		name: "Google",
-		amount: "$1,000",
-		date: "Wed 5:00pm",
-		status: "paid",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-netflix.svg",
-		name: "netflix",
-		amount: "$14,000",
-		date: "Wed 3:30am",
-		status: "cancelled",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-spotify.svg",
-		name: "Spotify",
-		amount: "$2,500",
-		date: "Wed 3:00pm",
-		status: "paid",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-amazon.svg",
-		name: "Amazon",
-		amount: "$5,000",
-		date: "Wed 1:00pm",
-		status: "paid",
-		account: "master-card",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-pinterest.svg",
-		name: "Pinterest",
-		amount: "$3,400",
-		date: "Mon 7:40pm",
-		status: "pending",
-		account: "master-card",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-google.svg",
-		name: "Google",
-		amount: "$1,000",
-		date: "Wed 5:00pm",
-		status: "paid",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-netflix.svg",
-		name: "netflix",
-		amount: "$14,000",
-		date: "Wed 3:30am",
-		status: "cancelled",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-];
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function ViewReminders() {
+	const [reminders, setReminders] = useState([]);
+	const apiUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/reminder/getReminder`;
+
+	useEffect(() => {
+		const fetchReminders = async () => {
+			try {
+				const accessToken = localStorage.getItem("accessToken");
+				if (!accessToken) {
+					console.error("Access token not found");
+					return;
+				}
+
+				const response = await axios.get(apiUrl, {
+					headers: {
+						Authorization: `Bearer ${accessToken}`,
+					},
+				});
+
+				if (response.data.success) {
+					setReminders(response.data.data);
+				} else {
+					console.error("Failed to fetch reminders:", response.data.message);
+				}
+			} catch (error) {
+				console.error("Error fetching reminders:", error);
+			}
+		};
+
+		fetchReminders();
+	}, []);
+
+	const handleCheckboxChange = async (reminderId, isChecked) => {
+		try {
+			const accessToken = localStorage.getItem("accessToken");
+			if (!accessToken) {
+				console.error("Access token not found");
+				return;
+			}
+
+			const response = await axios.patch(
+				`${apiUrl}/${reminderId}`,
+				{ isCompleted: isChecked },
+				{
+					headers: {
+						Authorization: `Bearer ${accessToken}`,
+					},
+				},
+			);
+
+			if (response.data.success) {
+				// Update the status locally
+				setReminders((prevReminders) =>
+					prevReminders.map((reminder) =>
+						reminder._id === reminderId
+							? { ...reminder, isCompleted: isChecked }
+							: reminder,
+					),
+				);
+			} else {
+				console.error(
+					"Failed to update reminder status:",
+					response.data.message,
+				);
+			}
+		} catch (error) {
+			console.error("Error updating reminder status:", error);
+		}
+	};
+
 	return (
-		<Card className="h-screen w-[96%] bg-[#1d1d1d]">
+		<Card className="w-[96%] bg-[#1d1d1d] p-5">
 			<CardHeader
 				floated={false}
 				shadow={false}
@@ -145,10 +87,7 @@ export function ViewReminders() {
 			>
 				<div className="mb-4 flex flex-col justify-between gap-8 md:flex-row md:items-center ">
 					<Typography variant="h4" color="light-green">
-						Recent Budget
-					</Typography>
-					<Typography color="light-green" className="mt-1 font-normal">
-						These are details about the last transactions
+						Recent Reminders
 					</Typography>
 				</div>
 			</CardHeader>
@@ -156,7 +95,7 @@ export function ViewReminders() {
 				<table className="w-full min-w-max table-auto text-left">
 					<thead>
 						<tr>
-							{TABLE_HEAD.map((head) => (
+							{["Title", "Amount", "Date", "Status"].map((head) => (
 								<th
 									key={head}
 									className="border-y border-blue-gray-100 bg-slate-600 p-4"
@@ -173,166 +112,57 @@ export function ViewReminders() {
 						</tr>
 					</thead>
 					<tbody>
-						{TABLE_ROWS.map(
-							(
-								{
-									img,
-									name,
-									amount,
-									date,
-									status,
-									account,
-									accountNumber,
-									expiry,
-									type,
-								},
-								index,
-							) => {
-								const isLast = index === TABLE_ROWS.length - 1;
-								const classes = isLast
-									? "p-4"
-									: "p-4 border-b border-blue-gray-50";
-
-								return (
-									<tr key={name}>
-										<td className={classes}>
-											<div className="flex items-center gap-3">
-												<Avatar
-													src={img}
-													alt={name}
-													size="md"
-													className="border border-blue-gray-50 object-contain p-1"
-												/>
-												<Typography
-													variant="small"
-													color="white"
-													className="font-bold"
-												>
-													{name}
-												</Typography>
-											</div>
-										</td>
-										<td className={classes}>
-											<Typography
-												variant="small"
-												color="white"
-												className="font-normal"
-											>
-												{amount}
-											</Typography>
-										</td>
-										<td className={classes}>
-											<Typography
-												variant="small"
-												color="white"
-												className="font-normal"
-											>
-												{date}
-											</Typography>
-										</td>
-										<td className={classes}>
-											<div className="w-max">
-												<Chip
-													size="sm"
-													variant="ghost"
-													value={status}
-													color={
-														status === "paid"
-															? "light-green"
-															: status === "pending"
-															? "amber"
-															: "red"
-													}
-												/>
-											</div>
-										</td>
-										<td className={classes}>
-											<Typography
-												variant="small"
-												color="white"
-												className="font-normal"
-											>
-												{type}
-											</Typography>
-										</td>
-										<td className={classes}>
-											<div className="flex items-center gap-3">
-												<div className="h-9 w-12 rounded-md border border-blue-gray-50 p-1">
-													<Avatar
-														src={
-															account === "visa"
-																? "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/visa.png"
-																: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/mastercard.png"
-														}
-														size="sm"
-														alt={account}
-														variant="square"
-														className="h-full w-full object-contain p-1"
-													/>
-												</div>
-												<div className="flex flex-col">
-													<Typography
-														variant="small"
-														color="white"
-														className="font-normal capitalize"
-													>
-														{account.split("-").join(" ")} {accountNumber}
-													</Typography>
-													<Typography
-														variant="small"
-														color="white"
-														className="font-normal opacity-70"
-													>
-														{expiry}
-													</Typography>
-												</div>
-											</div>
-										</td>
-										<td className={classes}>
-											<Tooltip content="Edit User">
-												<IconButton variant="text" className="text-white">
-													<PencilIcon className="h-4 w-4" />
-												</IconButton>
-											</Tooltip>
-										</td>
-									</tr>
-								);
-							},
-						)}
+						{reminders.map(({ _id, title, amount, createdAt, isCompleted }) => (
+							<tr key={_id}>
+								<td className="p-4 border-b border-blue-gray-50">
+									<div className="flex items-center gap-3">
+										<input
+											type="checkbox"
+											checked={isCompleted}
+											onChange={(e) =>
+												handleCheckboxChange(_id, e.target.checked)
+											}
+										/>
+										<Typography
+											variant="small"
+											color="white"
+											className="font-bold"
+										>
+											{title}
+										</Typography>
+									</div>
+								</td>
+								<td className="p-4 border-b border-blue-gray-50">
+									<Typography
+										variant="small"
+										color="white"
+										className="font-normal"
+									>
+										{amount || "-"}
+									</Typography>
+								</td>
+								<td className="p-4 border-b border-blue-gray-50">
+									<Typography
+										variant="small"
+										color="white"
+										className="font-normal"
+									>
+										{new Date(createdAt).toLocaleString()}
+									</Typography>
+								</td>
+								<td className="border-b border-blue-gray-50">
+									<Chip
+										size="sm"
+										variant="ghost"
+										value={isCompleted ? "Completed" : "Pending"}
+										color={isCompleted ? "light-green" : "amber"}
+									/>
+								</td>
+							</tr>
+						))}
 					</tbody>
 				</table>
 			</CardBody>
-			<CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4 text-white">
-				<Button variant="outlined" size="sm" color="white">
-					Previous
-				</Button>
-				<div className="flex items-center gap-2">
-					<IconButton variant="outlined" size="sm" color="white">
-						1
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						2
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						3
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						...
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						8
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						9
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						10
-					</IconButton>
-				</div>
-				<Button variant="outlined" size="sm" color="white">
-					Next
-				</Button>
-			</CardFooter>
 		</Card>
 	);
 }
