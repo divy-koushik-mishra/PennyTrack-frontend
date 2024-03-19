a1305a5628b4895f61f2460ee5c4e1f2f7911bdb
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-19 18:50:39 +0530

    add-income-api-integrated

--- components/dashboard/Income/AddIncome.jsx
+++ components/dashboard/Income/AddIncome.jsx
@@ -1,15 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function AddIncome() {
+	const [formData, setFormData] = useState({
+		income_description: "",
+		income_category: "",
+		income_amount: "",
+	});
+
+	const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/income/addIncome`;
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			[name]: value,
+		}));
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const accessToken = localStorage.getItem("accessToken");
+			if (!accessToken) {
+				return;
+			}
+
+			const config = {
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			};
+
+			const response = await axios.post(url, formData, config);
+
+			console.log("Income added successfully:", response.data);
+
+			setFormData({
+				income_description: "",
+				income_category: "",
+				income_amount: "",
+			});
+		} catch (error) {
+			console.error("Error adding income:", error);
+		}
+	};
+
 	return (
 		<div className="text-white">
-			<form className="lg:flex flex-col m-4 lg:mx-[20rem]">
+			<form
+				className="lg:flex flex-col m-4 lg:mx-[20rem]"
+				onSubmit={handleSubmit}
+			>
 				<div className="">
 					{/* category */}
 					<div className="mb-6">
 						<p className="text-lg mb-6">Income Category</p>
 						<select
 							type="text"
+							name="income_category"
+							value={formData.income_category}
+							onChange={handleChange}
 							className="w-full h-[3.25rem] bg-[#292929] border border-[#333333] rounded-md p-4"
 						>
 							<option value="work">Work</option>
@@ -25,6 +76,9 @@ export default function AddIncome() {
 						<p className="text-lg mb-6">Income Description</p>
 						<input
 							type="text"
+							name="income_description"
+							value={formData.income_description}
+							onChange={handleChange}
 							className="w-full h-[3.25rem] bg-[#292929] border border-[#333333] rounded-md p-4"
 						/>
 					</div>
@@ -33,6 +87,9 @@ export default function AddIncome() {
 						<p className="text-lg mb-6">Income Amount</p>
 						<input
 							type="number"
+							name="income_amount"
+							value={formData.income_amount}
+							onChange={handleChange}
 							className="w-full h-[3.25rem] bg-[#292929] border border-[#333333] rounded-md p-4"
 						/>
 					</div>
@@ -44,7 +101,10 @@ export default function AddIncome() {
 							className="w-full h-[3.25rem] bg-[#292929] border border-[#333333] rounded-md p-4"
 						/>
 					</div>
-					<button className="w-full flex justify-center items-center bg-gradient-to-r from-light-green-800 via-light-green-500 to-light-green-300 text-black text-xl font-medium rounded-[0.25rem] py-[0.6875rem] pr-4 pl-6 transition duration-300 ease-in-out hover:shadow-top-md hover:shadow-bottom-md">
+					<button
+						type="submit"
+						className="w-full flex justify-center items-center bg-gradient-to-r from-light-green-800 via-light-green-500 to-light-green-300 text-black text-xl font-medium rounded-[0.25rem] py-[0.6875rem] pr-4 pl-6 transition duration-300 ease-in-out hover:shadow-top-md hover:shadow-bottom-md"
+					>
 						Add Income
 					</button>
 				</div>
