8c64942f7113325be6e19fc6020a6daac787d048
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-19 19:21:58 +0530

    dashboard-ui-changes

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -7,33 +7,36 @@ import Topbar from "@/components/dashboard/Homepage/Topbar";
 import { TransactionsTable } from "@/components/dashboard/Homepage/TransactionsTable";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { TransactionsPageTable } from "@/components/dashboard/Transaction/TransactioPageTable";
+import ReminderTabs from "@/components/dashboard/Reminders/ReminderTabs";
+import { ViewReminders } from "@/components/dashboard/Reminders/ViewReminders";
 export default function Dashboard() {
-  const router = useRouter();
-  if (typeof window !== "undefined") {
-    const accessToken = localStorage.getItem("accessToken");
-    if (!accessToken) {
-      router.push("/login");
-    }
-  }
-  return (
-    <div className="flex flex-col">
-      <div className="flex">
-        <div className="sticky">
-          <Sidebar />
-        </div>
-        <div className="flex flex-col w-full space-x-6">
-          <Topbar />
-          <IEBCards />
-          <div className="flex gap-x-4 pt-6">
-            <Chart />
-            <Budgeting />
-          </div>
-          <div className="flex gap-x-4 pt-6 mb-4">
-            <TransactionsTable />
-            <Reminders />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const router = useRouter();
+	if (typeof window !== "undefined") {
+		const accessToken = localStorage.getItem("accessToken");
+		if (!accessToken) {
+			router.push("/login");
+		}
+	}
+	return (
+		<div className="flex flex-col">
+			<div className="flex">
+				<div className="sticky">
+					<Sidebar />
+				</div>
+				<div className="flex flex-col w-full space-x-6">
+					<Topbar />
+					<IEBCards />
+					<div className="flex gap-x-4 pt-6">
+						<Chart />
+						<Budgeting />
+					</div>
+					<div className="flex gap-x-4 pt-6 mb-4">
+						<TransactionsPageTable />
+						<ViewReminders />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
