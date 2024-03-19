52a51ea0964b5c4d41a71caf634912527f5ec8b9
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-03-19 20:09:58 +0530

    budget-get-request-integrated

--- components/dashboard/Budgeting/ViewBudget.jsx
+++ components/dashboard/Budgeting/ViewBudget.jsx
@@ -1,143 +1,67 @@
-import { PencilIcon } from "@heroicons/react/24/solid";
 import {
-	Avatar,
-	Button,
 	Card,
 	CardBody,
-	CardFooter,
 	CardHeader,
-	Chip,
-	IconButton,
-	Tooltip,
 	Typography,
 } from "@material-tailwind/react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const TABLE_HEAD = [
-	"Transaction",
+	"Title",
+	"Description",
+	"Category",
 	"Amount",
-	"Date",
-	"Status",
-	"Type",
-	"Account",
-	"",
-];
-
-const TABLE_ROWS = [
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-spotify.svg",
-		name: "Spotify",
-		amount: "$2,500",
-		date: "Wed 3:00pm",
-		status: "paid",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-amazon.svg",
-		name: "Amazon",
-		amount: "$5,000",
-		date: "Wed 1:00pm",
-		status: "paid",
-		account: "master-card",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-pinterest.svg",
-		name: "Pinterest",
-		amount: "$3,400",
-		date: "Mon 7:40pm",
-		status: "pending",
-		account: "master-card",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-google.svg",
-		name: "Google",
-		amount: "$1,000",
-		date: "Wed 5:00pm",
-		status: "paid",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-netflix.svg",
-		name: "netflix",
-		amount: "$14,000",
-		date: "Wed 3:30am",
-		status: "cancelled",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-spotify.svg",
-		name: "Spotify",
-		amount: "$2,500",
-		date: "Wed 3:00pm",
-		status: "paid",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-amazon.svg",
-		name: "Amazon",
-		amount: "$5,000",
-		date: "Wed 1:00pm",
-		status: "paid",
-		account: "master-card",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-pinterest.svg",
-		name: "Pinterest",
-		amount: "$3,400",
-		date: "Mon 7:40pm",
-		status: "pending",
-		account: "master-card",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-google.svg",
-		name: "Google",
-		amount: "$1,000",
-		date: "Wed 5:00pm",
-		status: "paid",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
-	{
-		img: "https://docs.material-tailwind.com/img/logos/logo-netflix.svg",
-		name: "netflix",
-		amount: "$14,000",
-		date: "Wed 3:30am",
-		status: "cancelled",
-		account: "visa",
-		accountNumber: "1234",
-		expiry: "06/2026",
-		type: "Debit",
-	},
+	"Threshold Amount",
 ];
 
 export function ViewBudget() {
+	const [budget, setBudget] = useState([]);
+	const apiUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/budget/getBudget`;
+
+	useEffect(() => {
+		const fetchBudget = async () => {
+			try {
+				const accessToken = localStorage.getItem("accessToken");
+				if (!accessToken) {
+					console.error("Access token not found");
+					return;
+				}
+
+				const response = await axios.post(
+					apiUrl,
+					{},
+					{
+						headers: {
+							Authorization: `Bearer ${accessToken}`,
+						},
+					},
+				);
+
+				if (response.data.success) {
+					setBudget(response.data.data);
+				} else {
+					console.error("Failed to fetch Budget:", response.data.message);
+				}
+			} catch (error) {
+				console.error("Error fetching Budget:", error);
+			}
+		};
+
+		fetchBudget();
+	}, []);
+
+	// Function to format date to DD-MM-YYYY
+	const formatDate = (dateString) => {
+		const date = new Date(dateString);
+		const day = date.getDate().toString().padStart(2, "0");
+		const month = (date.getMonth() + 1).toString().padStart(2, "0");
+		const year = date.getFullYear();
+		return `${day}-${month}-${year}`;
+	};
+
 	return (
-		<Card className="h-screen w-[96%] bg-[#1d1d1d]">
+		<Card className="h-screen w-[96%] bg-[#1d1d1d] p-5">
 			<CardHeader
 				floated={false}
 				shadow={false}
@@ -145,7 +69,7 @@ export function ViewBudget() {
 			>
 				<div className="mb-4 flex flex-col justify-between gap-8 md:flex-row md:items-center ">
 					<Typography variant="h4" color="light-green">
-						Recent Budget
+						Recent Budgets
 					</Typography>
 					<Typography color="light-green" className="mt-1 font-normal">
 						These are details about the last transactions
@@ -173,166 +97,69 @@ export function ViewBudget() {
 						</tr>
 					</thead>
 					<tbody>
-						{TABLE_ROWS.map(
-							(
-								{
-									img,
-									name,
-									amount,
-									date,
-									status,
-									account,
-									accountNumber,
-									expiry,
-									type,
-								},
-								index,
-							) => {
-								const isLast = index === TABLE_ROWS.length - 1;
-								const classes = isLast
-									? "p-4"
-									: "p-4 border-b border-blue-gray-50";
-
-								return (
-									<tr key={name}>
-										<td className={classes}>
-											<div className="flex items-center gap-3">
-												<Avatar
-													src={img}
-													alt={name}
-													size="md"
-													className="border border-blue-gray-50 object-contain p-1"
-												/>
-												<Typography
-													variant="small"
-													color="white"
-													className="font-bold"
-												>
-													{name}
-												</Typography>
-											</div>
-										</td>
-										<td className={classes}>
+						{budget.map(
+							({
+								_id,
+								budget_category,
+								budget_title,
+								budget_description,
+								budget_amount,
+								threshold_amount,
+							}) => (
+								<tr key={_id}>
+									<td className="p-4 border-b border-blue-gray-50">
+										<div className="flex items-center gap-3">
 											<Typography
 												variant="small"
 												color="white"
-												className="font-normal"
+												className="font-bold"
 											>
-												{amount}
+												{budget_title}
 											</Typography>
-										</td>
-										<td className={classes}>
-											<Typography
-												variant="small"
-												color="white"
-												className="font-normal"
-											>
-												{date}
-											</Typography>
-										</td>
-										<td className={classes}>
-											<div className="w-max">
-												<Chip
-													size="sm"
-													variant="ghost"
-													value={status}
-													color={
-														status === "paid"
-															? "light-green"
-															: status === "pending"
-															? "amber"
-															: "red"
-													}
-												/>
-											</div>
-										</td>
-										<td className={classes}>
-											<Typography
-												variant="small"
-												color="white"
-												className="font-normal"
-											>
-												{type}
-											</Typography>
-										</td>
-										<td className={classes}>
-											<div className="flex items-center gap-3">
-												<div className="h-9 w-12 rounded-md border border-blue-gray-50 p-1">
-													<Avatar
-														src={
-															account === "visa"
-																? "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/visa.png"
-																: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/mastercard.png"
-														}
-														size="sm"
-														alt={account}
-														variant="square"
-														className="h-full w-full object-contain p-1"
-													/>
-												</div>
-												<div className="flex flex-col">
-													<Typography
-														variant="small"
-														color="white"
-														className="font-normal capitalize"
-													>
-														{account.split("-").join(" ")} {accountNumber}
-													</Typography>
-													<Typography
-														variant="small"
-														color="white"
-														className="font-normal opacity-70"
-													>
-														{expiry}
-													</Typography>
-												</div>
-											</div>
-										</td>
-										<td className={classes}>
-											<Tooltip content="Edit User">
-												<IconButton variant="text" className="text-white">
-													<PencilIcon className="h-4 w-4" />
-												</IconButton>
-											</Tooltip>
-										</td>
-									</tr>
-								);
-							},
+										</div>
+									</td>
+									<td className="p-4 border-b border-blue-gray-50">
+										<Typography
+											variant="small"
+											color="white"
+											className="font-normal"
+										>
+											{budget_description}
+										</Typography>
+									</td>
+									<td className="p-4 border-b border-blue-gray-50">
+										<Typography
+											variant="small"
+											color="white"
+											className="font-normal"
+										>
+											{budget_category}
+										</Typography>
+									</td>
+									<td className="p-4 border-b border-blue-gray-50">
+										<Typography
+											variant="small"
+											color="white"
+											className="font-normal"
+										>
+											{budget_amount || "-"}
+										</Typography>
+									</td>
+									<td className="p-4 border-b border-blue-gray-50">
+										<Typography
+											variant="small"
+											color="white"
+											className="font-normal"
+										>
+											{threshold_amount || "-"}
+										</Typography>
+									</td>
+								</tr>
+							),
 						)}
 					</tbody>
 				</table>
 			</CardBody>
-			<CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4 text-white">
-				<Button variant="outlined" size="sm" color="white">
-					Previous
-				</Button>
-				<div className="flex items-center gap-2">
-					<IconButton variant="outlined" size="sm" color="white">
-						1
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						2
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						3
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						...
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						8
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						9
-					</IconButton>
-					<IconButton variant="text" size="sm" color="white">
-						10
-					</IconButton>
-				</div>
-				<Button variant="outlined" size="sm" color="white">
-					Next
-				</Button>
-			</CardFooter>
 		</Card>
 	);
 }
